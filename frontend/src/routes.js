3d6832ce35f7dfbd75bd65927fe875b0a9eb04c7
Lucas Cardoso <lucascoronel@MacBook-Pro-de-Lucas.local>
2020-01-10 19:38:59 -0200

    chore: fronted - Web is ready!

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import Login from './pages/Login';
@@ -8,7 +9,9 @@ export default function Routes(){
   return (
     <BrowserRouter>
       <Switch>
-
+        <Route path="/" exact component={Login}/>
+        <Route path="/dashboard" exact component={Dashboard}/>
+        <Route path="/new" exact component={New}/>
       </Switch>
     </BrowserRouter>
   );
